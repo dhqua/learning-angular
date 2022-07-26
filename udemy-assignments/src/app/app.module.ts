616d1abfc99ab05529f5d5716b8be1ce81b48f19
dhqua <quaon.thomas1@gmail.com>
2022-07-26 17:31:18 -0500

    added assignment 3 component

--- udemy-assignments/src/app/app.module.ts
+++ udemy-assignments/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { TitlePipe } from './title.pipe';
 import { ListService } from './list.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,12 +6,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AuthorsComponent } from './authors/authors.component';
 import { StarButtonComponent } from './star-button/star-button.component';
+import { TestCustomPipeComponent } from './test-custom-pipe/test-custom-pipe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthorsComponent,
-    StarButtonComponent
+    StarButtonComponent,
+    TitlePipe,
+    TestCustomPipeComponent
   ],
   imports: [
     BrowserModule
---
+++ udemy-assignments/src/app/test-custom-pipe/test-custom-pipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-custom-pipe',
+  templateUrl: './test-custom-pipe.component.html',
+  styleUrls: ['./test-custom-pipe.component.css']
+})
+export class TestCustomPipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
